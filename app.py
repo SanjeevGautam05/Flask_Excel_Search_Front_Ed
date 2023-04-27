231aecd9647a0aeab4c0b15cd347decc398d3515
Sanjeev Gautam <59804721+SanjeevGautam05@users.noreply.github.com>
2023-04-27 08:28:42 -0400

    Enhancement

--- app.py
+++ app.py
@@ -3,30 +3,36 @@ from flask import Flask, render_template, request
 from flask_caching import Cache
 
 app = Flask(__name__)
-cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+# cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 # Define the route for the main page
 @app.route('/')
-@cache.cached(timeout=60)
+# @cache.cached(timeout=60)
 
 def index():
     # Read the Excel file and select the worksheet
-    wb = openpyxl.load_workbook('Salaries.xlsx')
+    wb = openpyxl.load_workbook('Test.xlsx')
     ws = wb.active
 
     # Get the headers for the Excel data
     headers = [cell.value for cell in ws[1]]
 
+    data = []
+    for row in ws.iter_rows(min_row=2, max_row=6):
+            print(row[0].value)
+            data.append([cell.value for cell in row])
+            # print(data)
+
     # Render the HTML template with the headers and empty data
-    return render_template('index.html', headers=headers, data=[])
+    return render_template('index.html', headers=headers, data=data)
 
 # Define the route for the search functionality
 @app.route('/search', methods=['GET', 'POST'])
-@cache.cached(timeout=60)
+# @cache.cached(timeout=60)
 
 def search():
     # Read the Excel file and select the worksheet
-    wb = openpyxl.load_workbook('Salaries.xlsx')
+    wb = openpyxl.load_workbook('Test.xlsx')
     ws = wb.active
 
     # Get the headers for the Excel data
@@ -38,7 +44,7 @@ def search():
     # Filter the data based on the search term
     data = []
     for row in ws.iter_rows(min_row=2):
-        if search_term.lower() in row[1].value.lower():
+        if (search_term.lower() in row[2].value.lower()) or (search_term.lower() in row[4].value.lower()):
             data.append([cell.value for cell in row])
 
     # Render the HTML template with the headers and filtered data
